e1db1bf69096a0ff4068d0a3846c357858e8a244
radiate-positivity <45203491+radiate-positivity@users.noreply.github.com>
2019-04-28 09:35:01 +0400

    Update app.py

--- app.py
+++ app.py
@@ -74,8 +74,6 @@ def handle_dialog(res, req):
                 sessionStorage[user_id]['game_id'] = game_id
                 sessionStorage[user_id]['game_started'] = True
                 #res['response']['text'] = game_id
-                p, p_o, card = take(sessionStorage[user_id]['game_id'], sessionStorage[user_id]['point'], res)
-                sessionStorage[user_id]['point'] = p
                 play_game(res, req)
 
             elif 'нет' in req['request']['nlu']['tokens']:
